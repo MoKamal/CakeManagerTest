c32165f8d7e1552352b73555a8583cd856712268
Mohammed Hussein <>
2017-11-17 05:22:45 +0000

    Initial Comit

---
+++ src/main/java/com/waracle/cakemgr/exception/CakeManagerException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.waracle.cakemgr.exception;
+
+/**
+ *
+ * @author DELL 5559
+ */
+public class CakeManagerException extends RuntimeException {
+    
+    public CakeManagerException(String message) {
+        super(message);
+    }
+}
+    
